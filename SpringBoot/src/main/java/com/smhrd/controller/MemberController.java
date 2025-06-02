7c6519893cf940218c03fa41c4adbe8d2e5a550c
plume213 <iyd3378@gmail.com>
2025-06-02 12:39:36 +0900

    전 버전으로 롤백

--- SpringBoot/src/main/java/com/smhrd/controller/MemberController.java
+++ SpringBoot/src/main/java/com/smhrd/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.smhrd.controller; // 실제 패키지 경로로 수정
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.smhrd.entity.Member;
@@ -13,29 +14,33 @@ import jakarta.servlet.http.HttpSession;
 public class MemberController {
 
 	@Autowired
-    private MemberService memberService;
+	private MemberService memberService;
 
 	@GetMapping("/mypage")
-	public String mypage() {
-		return "mypage";
+	public String mypage(HttpSession session, Model model) {
+		Member member = (Member) session.getAttribute("info");
+		if (member != null) {
+			model.addAttribute("member", member);
+		}
+		return "mypage"; // mypage.html or mypage.jsp
 	}
-	
+
 	@GetMapping("/update")
 	public String update() {
 		return "update";
 	}
-	
+
 	@GetMapping("/list")
 	public String list(HttpSession session) {
-		
+
 		Member info = (Member) session.getAttribute("info");
-		
-		if(info != null && info.getId().equals("admin")) {
+
+		if (info != null && info.getId().equals("admin")) {
 			return "list";
 		} else {
 			return "redirect:/";
 		}
-		
+
 	}
-	
+
 }
