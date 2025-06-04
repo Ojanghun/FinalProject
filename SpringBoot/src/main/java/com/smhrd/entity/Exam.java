4094934815a2a3405a1de37039e2f3b0e3ea9331
Ojanghun <janghun1527@gmail.com>
2025-06-04 11:26:15 +0900

    OJH
    ojh update

--- SpringBoot/src/main/java/com/smhrd/entity/Exam.java
+++ SpringBoot/src/main/java/com/smhrd/entity/Exam.java
@@ -3,52 +3,55 @@ package com.smhrd.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "PB_INFO")
 public class Exam {
 	
 	@Id
-	@Column(name = "PB_ID", unique = true, nullable = false)
+	@Column(name = "PB_IDX", unique = true, nullable = false)
 	private int pbId; // 문제 아이디 - PK
 
-	@Column(name = "EX_ID", nullable = false)
-	private int exId; // 시험 아이디 - 출제 년도 별 묶음
-
+	@Column(name = "LI_IDX", nullable = false) 
+    private int liIdx; // 자격증 인덱스
+	
+	@Column(name = "EX_IDX", nullable = false) 
+    private int exId; // 시험 인덱스
+	
 	@Column(name = "PB_NUM", nullable = false)
 	private int pbNum; // 문제 번호 - 출제 년도 별 문제 번호
 
 	@Column(name = "PB_QUES", length = 1000, nullable = false)
 	private String pbQues; // 문제 질문
 
-	@Column(name = "PB_DETAIL")
-	private byte[] pbDetail; // 문제 상세설명
+	@Column(name = "PB_DETAIL", length = 1000)
+	private String pbDetail; // 문제 상세설명 → 왜 문자열이 아니지?
 
-	@Column(name = "PB_CHOI1", length = 255, nullable = false)
+	@Column(name = "PB_CHOI1", length = 1000, nullable = false)
 	private String pbChoi1; // 문제 선지1
 
-	@Column(name = "PB_CHOI2", length = 255, nullable = false)
+	@Column(name = "PB_CHOI2", length = 1000, nullable = false)
 	private String pbChoi2; // 문제 선지2
 
-	@Column(name = "PB_CHOI3", length = 255, nullable = false)
+	@Column(name = "PB_CHOI3", length = 1000, nullable = false)
 	private String pbChoi3; // 문제 선지3
 
-	@Column(name = "PB_CHOI4", length = 255, nullable = false)
+	@Column(name = "PB_CHOI4", length = 1000, nullable = false)
 	private String pbChoi4; // 문제 선지4
 
 	@Column(name = "PB_ANS", nullable = false)
 	private int pbAns; // 정답 번호
 
-	@Column(name = "PB_SOLU", length = 1000, nullable = false)
+	@Column(name = "PB_SOLU", length = 2000, nullable = false)
 	private String pbSolu; // 해설
 
 	@Column(name = "PB_CR", nullable = false)
 	private String pbCr; // 문제 정답률
 
-	@Column(name = "PB_TOPIC", length = 255, nullable = false)
-	private int pbTopic; // 주제 - 문제 유형 0~56
-
-
-
+	@Column(name = "TOPIC_IDX", nullable = false) 
+    private int pbTopic;
+	
 }
---
+++ SpringBoot/src/main/java/com/smhrd/entity/Li_Info.java
@@ -0,0 +1,31 @@
+package com.smhrd.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "LI_INFO")
+public class Li_Info {
+
+    @Id
+    @Column(name = "LI_IDX", nullable = false, unique = true) 
+    private int liIdx; // 자격증 인덱스
+    
+    @Column(name = "LI_NAME", nullable = false, length = 50) 
+    private String liName; // 자격증 이름
+    
+    @Column(name = "LI_TYPE", nullable = false)
+    private Boolean liType; // 자격증 타입(필기: 0, 실기: 1)
+    
+    @Column(name = "LI_TIME", nullable = false) 
+    private int liTime; // 자격증 시험시간 인덱스
+    
+}
