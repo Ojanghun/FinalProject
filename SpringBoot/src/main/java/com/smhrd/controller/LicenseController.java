92fe8262575f66c40d393b9e43cd1c28f7619b12
SonJonghee <jonghee1027@gmail.com>
2025-06-05 14:36:49 +0900

    다시 수정

--- SpringBoot/src/main/java/com/smhrd/controller/LicenseController.java
+++ SpringBoot/src/main/java/com/smhrd/controller/LicenseController.java
@@ -1,13 +1,19 @@
 package com.smhrd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smhrd.service.LIcenseService;
+
 @Controller
 public class LicenseController {
+	
+	@Autowired
+	LIcenseService service;
 	
 	@RequestMapping("/goExam.do")
 	public String goExam(@RequestParam("category") int category, Model model) {
--- SpringBoot/src/main/java/com/smhrd/repository/LicenseRepository.java
+++ SpringBoot/src/main/java/com/smhrd/repository/LicenseRepository.java
@@ -1,9 +1,13 @@
 package com.smhrd.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.smhrd.entity.Atd_Log;
+
+
 @Repository
-public interface LicenseRepository {
+public interface LicenseRepository extends JpaRepository<Atd_Log, Integer> {
 
 	
 }
