7089e2a448dfef10aa49883b3dd3cdb75ce7c497
SonJonghee <jonghee1027@gmail.com>
2025-06-05 17:14:19 +0900

    라이센스에 Member 값 불러오기

--- SpringBoot/src/main/java/com/smhrd/controller/ProductController.java
+++ SpringBoot/src/main/java/com/smhrd/controller/ProductController.java
@@ -1,13 +1,22 @@
 package com.smhrd.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.smhrd.entity.Member;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ProductController {
 
 	@GetMapping("/license")
-	public String license() {
+	public String license(HttpSession session, Model model) {
+		Member member = (Member) session.getAttribute("info");
+		if (member != null) {
+			model.addAttribute("member", member);
+		}
 		return "license";
 	}
 	
