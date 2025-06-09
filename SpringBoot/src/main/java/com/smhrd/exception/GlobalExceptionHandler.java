44513f9185d57b2970d58405f4439ebc1d8b44b3
mosunghyun <mosunghyun@naver.com>
2025-06-09 11:42:38 +0900

    오류시 메인

---
+++ SpringBoot/src/main/java/com/smhrd/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.smhrd.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class) // 모든 예외 처리
+    public ModelAndView handleException(Exception ex, Model model) {
+        ex.printStackTrace(); // 로그 출력
+        return new ModelAndView("redirect:/main"); // 메인으로 이동
+    }
+}
