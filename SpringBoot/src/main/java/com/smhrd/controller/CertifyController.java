3977c5c2f7ac1b718f1f6f7b05579b1a16955c76
plume213 <iyd3378@gmail.com>
2025-06-11 17:40:14 +0900

    certify 페이지 자격증 이름, 예금주 자동입력

--- SpringBoot/src/main/java/com/smhrd/controller/CertifyController.java
+++ SpringBoot/src/main/java/com/smhrd/controller/CertifyController.java
@@ -1,16 +1,27 @@
 package com.smhrd.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.smhrd.entity.Pay_Info;
+import com.smhrd.entity.Member;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class CertifyController {
 
 	@GetMapping("/certify")
-	public String certify() {
-			return "certify";
+	public String certify(@RequestParam("liName") String liName, HttpSession session, Model model) {
+		Member member = (Member) session.getAttribute("info");
+		model.addAttribute("member", member);
+		model.addAttribute("liName", liName);
+		
+		// 전에 쓰던 Model 객체의 데이터를 다른 페이지로 넘길 순 없나?
+		// URL 파라미터로 넘기거나 Session에 담거나 아예 DB에서부터 조회
+		
+		return "certify";
 	}
-	
+
 }
