138007634181ab2e6c773f790a8004d8f7ff6400
SonJonghee <jonghee1027@gmail.com>
2025-06-11 12:18:10 +0900

    문제 회차 변경

--- SpringBoot/src/main/java/com/smhrd/repository/ExInfoRepository.java
+++ SpringBoot/src/main/java/com/smhrd/repository/ExInfoRepository.java
@@ -15,7 +15,7 @@ public interface ExInfoRepository extends JpaRepository<Ex_Info, Integer> {
 	
 	// ex_info 테이블에서 년도, 회차 중복 없이 가져옴
 	@Query("SELECT DISTINCT YEAR(e.exStd) AS year, e.exRound AS round, exId " +
-		       "FROM Ex_Info e ORDER BY YEAR(e.exStd), e.exRound")
+		       "FROM Ex_Info e WHERE e.exPbexist = 1 ORDER BY YEAR(e.exStd), e.exRound")
 		List<Object[]> findAllYearsAndRounds();
 
 }
